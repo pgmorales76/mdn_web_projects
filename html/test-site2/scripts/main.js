5f58289aa14e93c596a545935d9a95560fea5848
Peter G. Morales <81570648+pgmorales76@users.noreply.github.com>
2022-05-13 21:54:46 -0400

    Add files via upload

--- html/test-site2/scripts/main.js
+++ html/test-site2/scripts/main.js
@@ -1,1 +1,10 @@
+let myImage = document.querySelector('img');
 
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}

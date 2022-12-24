3187b3dbebbea3894b21cbe1b91e1dbd63fafa2f
Peter G. Morales <pgmorales76@gmail.com>
2022-12-24 18:05:35 -0500

    Added initial code setup for tutorial

---
+++ javascript/introduction_to_js_01/first_splash/scripts/number_guessing_game.js
@@ -0,0 +1,19 @@
+'use strict';
+
+console.log('Hello, World!');
+
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+
+function checkGuess() {
+    alert('I am a placeholder');
+}  
